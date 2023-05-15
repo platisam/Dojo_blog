3d2819f940a0ed0a2ea6a447004ef3d37c3e8adc
platisam <platisam@gmail.com>
2023-05-15 13:42:56 +0200

    added font and Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+    </div>
+  );
+};
+export default Home;
