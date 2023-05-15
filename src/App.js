bcf808e886df75afd5f59fb46914e5bda5b295d3
platisam <platisam@gmail.com>
2023-05-15 13:20:18 +0200

    added and styled Navbar component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-
-        <p>{10}</p>
-        <p>{"hello ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
+        <h1>App Component</h1>
       </div>
     </div>
   );
