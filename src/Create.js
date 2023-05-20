3ca8e8619f06a122eb11b20b25dea1424d2f816d
platisam <platisam@gmail.com>
2023-05-20 11:56:37 +0200

    added controlled inputs with styling create component

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
-  return <div className="create">Add a New Blog</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("marko");
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="marko">marko</option>
+          <option value="zarko">zarko</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 export default Create;
